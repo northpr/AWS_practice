d825e563c546b35ded4dedb03d4c6e25c09ec2c1
northpr <north_patawee@hotmail.com>
2022-06-29 00:14:46 +0200

    new update

--- 03_customers(dynamodb)/customer/customer_api/get.py
+++ 03_customers(dynamodb)/customer/customer_api/get.py
@@ -4,11 +4,10 @@ import os
 from boto3.dynamodb.conditions import Key
 
 
-
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('CUSTOMERS_TABLE')
 
 def lambda_handler(event,context):
-    dynamodb = boto3.resource('dynamodb')
-    table_name = os.environ.get('CUSTOMERS_TABLE')
     table = dynamodb.Table(table_name)
     customer_id = int(event['pathParameters']['id'])
     response = table.query(KeyConditionExpression=Key('id').eq(customer_id))
